fe9d8b5519910c224125b87fe93fb0f06dc08871
Nhu Nguyen <nhu313@gmail.com>
2018-06-08 12:14:55 -0400

    re-add files for part1

---
+++ src/test/java/com/zipcodewilmington/assessment2/ProjectTestSuite.java
@@ -0,0 +1,17 @@
+package com.zipcodewilmington.assessment2;
+
+import com.zipcodewilmington.assessment2.part1.WuTangConcatenatorTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by leon on 3/1/18.
+ * The purpose of this class is to test all methods in this project
+ */
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        WuTangConcatenatorTest.class,
+})
+public class ProjectTestSuite {
+}
